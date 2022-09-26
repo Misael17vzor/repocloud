26895cecc3dd2d7c6076285b8adec07b78f5f6c9
Misael Vazquez <misaelvzor@gmail.com>
2022-09-26 10:13:25 -0500

    mensaje de cambio desde casa

--- init.py
+++ init.py
@@ -1,1 +1,9 @@
 from odoo import fields, models, api
+#cambios
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    def _get_invoice_lines(self):
+        return self.mapped("invoice_line_ids")
+
